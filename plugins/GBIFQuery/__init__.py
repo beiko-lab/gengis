d185686b415bfe5d0d45252691788bc5b9dd47b1
Timothy <tymmek@gmail.com>
2013-07-05 13:35:12 -0300

    InnoInstaller update for 2.1.1 and GBIF init update
    Former-commit-id: 89f1cb1d1079b79a070c4e7900e40871d71be7f9

--- plugins/GBIFQuery/__init__.py
+++ plugins/GBIFQuery/__init__.py
@@ -8,13 +8,13 @@ def description():
 	return "Query GBIF DB for instances of Taxon Name in a region."
 	
 def authors():
-	return "Alexander Keddy"
+	return "Alexander Keddy & Timothy Mankowski"
 
 def publicationDate():
-	return "January 18, 2056"
+	return "July 4, 2013"
 	
 def minimumVersionOfGenGIS(): 
-	return "2.0"
+	return "2.1.1"
 
 def requireR():
 	return False
