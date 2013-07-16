4ed7b764692d8814e3b2d7cc633e62724c0b2dd4
Donovan Parks <donovan.parks@gmail.com>
2013-07-16 12:35:00 +1000

    Minor change to GBIF plugin. Added plugin headers.

--- plugins/Mantel/Mantel.py
+++ plugins/Mantel/Mantel.py
@@ -1,3 +1,24 @@
+#=======================================================================
+# Author: Donovan Parks
+#
+# Copyright 2012 Kathryn Dunphy, Rob Beiko, Donovan Parks
+#
+# This file is part of GenGIS.
+#
+# GenGIS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GenGIS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GenGIS.  If not, see <http://www.gnu.org/licenses/>.
+#=======================================================================
+
 from MantelLayout import MantelLayout
 from ScatterPlot import ScatterPlot
 
