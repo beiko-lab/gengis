fbac78fc6ad011d759eabeaba0ba4b432b044e59
akeddy <a.keddy.bcs@gmail.com>
2013-08-02 14:50:52 -0300

    set min/max map bounds for gbif
    Former-commit-id: 3e587d5205048e0acba8b4a98038a1341cac4799

--- plugins/GBIFQuery/GBIFQuery.py
+++ plugins/GBIFQuery/GBIFQuery.py
@@ -80,10 +80,10 @@ class GBIFQuery(GBIFQueryLayout):
 				self.m_MaxLon.SetValue(str(MaxLon))
 			else:
 				#Text boxes hate non String types. use int to round, and string to make them fit the container
-				self.m_MinLat.SetValue(str(borders.y1))
-				self.m_MaxLat.SetValue(str(borders.dy))
-				self.m_MinLon.SetValue(str(borders.x1))
-				self.m_MaxLon.SetValue(str(borders.dx))
+				self.m_MinLat.SetValue(str(max(MinLat,borders.y1)))
+				self.m_MaxLat.SetValue(str(min(MaxLat,borders.dy)))
+				self.m_MinLon.SetValue(str(max(MinLon,borders.x1)))
+				self.m_MaxLon.SetValue(str(min(MaxLat,borders.dx)))
 		
 	#	Query GBIF for Taxa in Lat/Lon Boundary
 	def OnSearch(self,event):
