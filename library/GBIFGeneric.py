afa8730425035268047d91260ced96af4626f6a7
akeddy <a.keddy.bcs@gmail.com>
2013-08-13 12:47:25 -0300

    fixed sorting and other changes
    removed defunct function
    removed some duplicate conversion
    removed legacy code that was useless

--- library/GBIFGeneric.py
+++ library/GBIFGeneric.py
@@ -23,19 +23,9 @@
 import re
 import wx
 import decimal
+from operator import itemgetter
 
 class GBIFGeneric:
-
-	def NOPEroundCoord(self,num):
-		string = str(num)
-		string = string.split('.')
-		#get first 3 numbers of string[1]
-		#find the floor of those numbers (nearest hundredth)
-		#cut off last digit
-		#put back together
-		backHalf = string[1][0:(min(len(string),2))]
-		num = string[0] + "." + backHalf
-		return(float(num))
 	
 	def roundCoord(self,num):
 		return round(decimal.Decimal(str(num)),1)
@@ -43,31 +33,40 @@ class GBIFGeneric:
 	def GETTEXT (self,obs_list,conversions_list):
 		OUTL=""
 		OUTS=""
-		for obs,convs in zip(obs_list,conversions_list):
-			locs, seqs = self.MAKEOUTS(obs,convs)
+	#	for obs,convs in zip(obs_list,conversions_list):
+		print len(obs_list)
+		for obs in obs_list:
+			locs, seqs = self.MAKEOUTS(obs)
+		#	locs, seqs = self.MAKEOUTS(obs,convs)
 			OUTL+=locs
 			OUTS+=seqs
 		return(OUTL,OUTS)
 		
 	#	Transforms the mined data into text to be output
-	def MAKEOUTS (self,obs,conversions):
+#	def MAKEOUTS (self,obs,conversions):
+	def MAKEOUTS (self,obs):
 		uniqueSiteID = set()
 		OUTLTEXT=""
 		OUTSTEXT=""
 		seqFileAgg = {}
 		for cellOut in sorted(obs.keys()):
+			print cellOut
 			if len(obs[cellOut].keys()) > 0:
 				for taxOut in sorted(obs[cellOut].keys()):
 					thisList=obs[cellOut][taxOut]
-					for ent in thisList:
-						fullLat = float(re.sub(r'\<.*?\>','',ent[1]))
-						fullLon = float(re.sub(r'\<.*?\>','',ent[2]))
+				#	print "%s\n"%thisList
+				#	for ent in thisList:
+					for ent in sorted(thisList,key=itemgetter(1,2)):
+						print ent
+						fullLat = ent[1]
+						fullLon = ent[2]
 						siteID = "%s_%f_%f" %(ent[3],fullLat,fullLon)
 						siteID = re.sub(' ','_',siteID)
 						if siteID not in uniqueSiteID:
 							uniqueSiteID.add(siteID)
-							OUTLTEXT += ("%s,%f,%f,%d,%d,%s,%s\n" % (siteID, fullLat, fullLon, len(obs[cellOut].keys()), cellOut,ent[3],taxOut ))
-						toKey = "%s,%f,%f,%s,%s,%s,%s" %(siteID, conversions[cellOut][0],conversions[cellOut][1],ent[3],taxOut,ent[1],ent[2])
+							OUTLTEXT += ("%s,%f,%f,%d,%d,%s,%s\n" % (siteID, fullLat, fullLon, len(obs[cellOut].keys()), cellOut+(int(fullLon) +180),ent[3],taxOut ))
+						#toKey = "%s,%f,%f,%s,%s,%s,%s" %(siteID, conversions[cellOut][0],conversions[cellOut][1],ent[3],taxOut,ent[1],ent[2])
+						toKey = "%s,%f,%f,%s,%s,%s,%s" %(siteID, fullLat,fullLon,ent[3],taxOut,ent[1],ent[2])
 						toKey = re.sub(r'\<.*?\>','',toKey)
 						try:
 							seqFileAgg[toKey].extend([ent[0]])
