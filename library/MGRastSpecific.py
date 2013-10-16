11b381e0e3541a6ce7c5aa4c1789b040aacab97c
akeddy <a.keddy.bcs@gmail.com>
2013-10-16 10:16:44 -0300

    mgRAST now mines all data from JSON file
    Former-commit-id: 5bb5f54ab00ce43dee16f3ac215e7d74d67b6e87

--- library/MGRastSpecific.py
+++ library/MGRastSpecific.py
@@ -25,6 +25,8 @@ import sys
 import urllib2
 import time
 import simplejson as json
+import copy
+import unicodedata
 from operator import itemgetter
 
 class MGRastSpecific:
@@ -127,10 +129,16 @@ class MGRastSpecific:
 			return False
 		if matrix['matrix_type'] == 'dense':
 			nodeList = self.DENSEHANDLER(matrix)
+			metaKeys = []
+			metaVals = []
 		else:
-			nodeList = self.SPARSEHANDLER(matrix)
+			nodeList,metaKeys,metaVals = self.SPARSEHANDLER(matrix)
 		if nodeList == False:
 			return {}
+		
+		metaValsString = ','.join(str(x) for x in metaVals)
+	
+	#	metaValsString = metaVals
 		for node in nodeList:
 			id = node[0]['id']
 			lat = node[1]['metadata']['sample']['data']['latitude']
@@ -141,16 +149,53 @@ class MGRastSpecific:
 			taxonomy = '|'.join(str(i) for i in node[0]['metadata'][rowMeta])
 			taxonomy = re.sub(",","",taxonomy)
 			try:
-				obs[currGrid][id].extend([(richness,lat,lon,taxonomy,projID)])
+				obs[currGrid][id].extend([(richness,lat,lon,taxonomy,projID,metaValsString)])
 			except KeyError:
 				try:
-					obs[currGrid].update({id: [(richness,lat,lon,taxonomy,projID)] })
+					obs[currGrid].update({id: [(richness,lat,lon,taxonomy,projID,metaValsString)] })
 				except KeyError:
-					obs[currGrid] = {id: [(richness,lat,lon,taxonomy,projID)] }
+					obs[currGrid] = {id: [(richness,lat,lon,taxonomy,projID,metaValsString)] }
 		m_Progress.WriteText("Recieved %d records.\n"%len(nodeList))
-		return obs
-			
+		return (obs, metaKeys)
+	
+	# Get all possible metadata from the JSON file
+	def GRABALLMETADATA(self,input,oldKey,metaKeys,metaVals):
+		if isinstance(input,dict):
+			for key,value in input.iteritems():
+				# only dictionairies store the keys, so creates a key trail so you know where each trait came from
+				if( oldKey != "temp" ):
+					tempKey = "%s.%s" %(oldKey,key)
+				else:
+					tempKey = key
+				tempKeys,tempVals = self.GRABALLMETADATA(value,tempKey,metaKeys,metaVals)
+			return(metaKeys,metaVals)
+		elif isinstance(input,list):
+			for elem in input:
+				index = input.index(elem)
+				tempKey = ("%s.%s") %(oldKey,index)
+				tempKeys,tempVals = self.GRABALLMETADATA(elem,tempKey,metaKeys,metaVals)
+			return(tempKeys,tempVals)
+		elif isinstance(input,unicode):
+			input = input.encode('utf-8')
+			input = re.sub(',',' ',input)
+			tempKeys,tempVals = self.GRABALLMETADATA(input,oldKey,metaKeys,metaVals)
+			return(tempKeys,tempVals)
+		else:
+			oldKey = oldKey.encode('utf-8')
+			if isinstance(input,str):
+				input = re.sub(',',' ',input)
+			metaKeys.append(oldKey)
+			metaVals.append(input)
+			return (oldKey,input)
+		
 	def SPARSEHANDLER(self,matrix):
+		ignore = ['data','rows','matrix_type']
+		miningMatrix = copy.deepcopy(matrix)
+		for i in ignore:
+			miningMatrix.pop(i, None)
+		metaKeys = [];
+		metaVals = [];	
+		metaKeys, metaVals = self.GRABALLMETADATA(miningMatrix,"temp",metaKeys,metaVals)
 		nodeList=[]
 		metaData = matrix['columns']
 		data = matrix['data']
@@ -162,7 +207,7 @@ class MGRastSpecific:
 			node = [id[row],metaData[col],val]
 			nodeList.append(node)
 		
-		return nodeList
+		return (nodeList,metaKeys,metaVals)
 	
 	def DENSEHANDLER(self,matrix):
 		wx.MessageBox("Unexpected data format.")
@@ -196,17 +241,16 @@ class MGRastSpecific:
 					for ent in sorted(thisList,key=itemgetter(1,2)):
 						fullLat = float(ent[1])
 						fullLon = float(ent[2])
-					#	siteID = "%s_%f_%f" %(taxOut,fullLat,fullLon)
-						# change siteID to study ID
-					#	siteID = re.sub(' ','_',siteID)
-						
 						# parse any commas in taxon
 						taxOut = re.sub(',','\",\"',taxOut)
 						#siteID is now the study
 						siteID = ent[4];
 						if siteID not in uniqueSiteID:
 							uniqueSiteID.add(siteID)
-							OUTLTEXT += ("%s,%f,%f,%s\n" % (siteID, fullLat, fullLon, cellOut+(int(fullLon) +180)))
+							metadata = ent[5].decode("utf-8")
+							metadata = unicodedata.normalize('NFKD',metadata).encode('ascii','ignore')
+							print metadata
+							OUTLTEXT += ("%s,%f,%f,%s,%s\n" % (siteID, fullLat, fullLon, cellOut+(int(fullLon) +180), metadata))
 						#toKey = "%s,%s,%f,%f,%s,%s" %(ent[4],siteID,fullLat,fullLon,ent[0],ent[3])
 						toKey = "%s,%s,%f,%f,%s,%s" %(taxOut,ent[4],fullLat,fullLon,ent[0],ent[3])
 						seqFile.append(toKey)
