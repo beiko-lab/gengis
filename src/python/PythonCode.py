bebc74af230532c31ee51cfbf0bd9f7e5c07d5eb
GenGIS Smith <gengis@wombat.research.cs.dal.ca>
2014-03-24 10:11:38 -0300

    testing package changes for MAC

--- src/python/PythonCode.py
+++ src/python/PythonCode.py
@@ -7,8 +7,9 @@ class PythonInterpreter(wx.Panel):
   The wx.Panel is eventually added to GenGIS on the C++ wxWidgets side.
   """
   def __init__(self, parent):
-    wx.Panel.__init__(self, parent, -1, style=wx.SUNKEN_BORDER)            # Call parent's __init__ function
-    pyInterpreter = shell.Shell(self, -1)                                  # This is the actual interactive Python console.
-    sizer = wx.BoxSizer(wx.VERTICAL)
-    sizer.Add(pyInterpreter, 1, wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, 10)  # Add the shell to the sizer.
-    self.SetSizer(sizer)
+	wx.Panel.__init__(self, parent, -1, style=wx.SUNKEN_BORDER)            # Call parent's __init__ function
+	pyInterpreter = shell.Shell(self, -1)                                  # This is the actual interactive Python console.
+	sizer = wx.BoxSizer(wx.VERTICAL)
+	sizer.Add(pyInterpreter, 1, wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, 10)  # Add the shell to the sizer.
+	self.SetSizer(sizer)
+	wx.MessageBox('Hey look it executed')
