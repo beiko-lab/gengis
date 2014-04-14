7ebd4a5bc6bbcffecf318df01a6fd71d79c7ecdc
GenGIS Smith <gengis@wombat.research.cs.dal.ca>
2014-04-14 14:46:28 -0300

    changes to pythoncode

--- src/python/PythonCode.py
+++ src/python/PythonCode.py
@@ -7,16 +7,11 @@ class PythonInterpreter(wx.Panel):
   The wx.Panel is eventually added to GenGIS on the C++ wxWidgets side.
   """
   def __init__(self, parent):
-	fh = "/Users/gengis/Desktop/slopyOutput.txt"
-	outFile = open(fh,'w')
-	outFile.write("well at least this code gets executed\n")
-
-	wx.Panel.__init__(self, parent, -1, style=wx.SUNKEN_BORDER)            # Call parent's __init__ function
-	outFile.write("check 1\n")
-	#pyInterpreter = shell.Shell(self, -1)                                  # This is the actual interactive Python console.
-	#sizer = wx.BoxSizer(wx.VERTICAL)
-	outFile.write("check 2\n")
-	#sizer.Add(pyInterpreter, 1, wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, 10)  # Add the shell to the sizer.
-	#self.SetSizer(sizer)
-	outFile.write("check 3\n")
-	#outFile.close()
+    wx.Panel.__init__(self, parent, -1, style=wx.SUNKEN_BORDER)            # Call parent's __init__ function
+    pyInterpreter = shell.Shell(self, -1)                                  # This is the actual interactive Python console.
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    outFile.write("check 2\n")
+    sizer.Add(pyInterpreter, 1, wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, 10)  # Add the shell to the sizer.
+    self.SetSizer(sizer)
+    outFile.write("check 3\n")
+    outFile.close()
