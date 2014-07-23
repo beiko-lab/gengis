f5a682ef56b087485dba76de0f446650ccf8d69f
akeddy <a.keddy.bcs@gmail.com>
2014-07-23 14:46:25 -0300

    removed print lines from PythonCode

--- src/python/PythonCode.py
+++ src/python/PythonCode.py
@@ -10,8 +10,8 @@ class PythonInterpreter(wx.Panel):
     wx.Panel.__init__(self, parent, -1, style=wx.SUNKEN_BORDER)            # Call parent's __init__ function
     pyInterpreter = shell.Shell(self, -1)                                  # This is the actual interactive Python console.
     sizer = wx.BoxSizer(wx.VERTICAL)
-    outFile.write("check 2\n")
+ #   outFile.write("check 2\n")
     sizer.Add(pyInterpreter, 1, wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, 10)  # Add the shell to the sizer.
     self.SetSizer(sizer)
-    outFile.write("check 3\n")
-    outFile.close()
+ #   outFile.write("check 3\n")
+ #   outFile.close()
