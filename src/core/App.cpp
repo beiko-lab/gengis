1a122f37f66211f667d80517dd08bba72be9d2b9
akeddy <a.keddy.bcs@gmail.com>
2014-11-10 12:52:17 -0400

    updated app version

--- src/core/App.cpp
+++ src/core/App.cpp
@@ -108,9 +108,7 @@ App::App():
 	m_bCurrentlySerializing( false ),
 	m_bSaveIsUpToDate( true ),
 	m_bIsEmptySession( true ),
-	//m_appVersion( wxT( "2.2.0" ) )
-	//m_appVersion( wxT( "2.2.1" ) )
-	m_appVersion( wxT( "2.3.0" ) )
+	m_appVersion( wxT( "2.4.0" ) )
 {
 	m_colourMapManager.reset(new ColourMapManager());
 	std::wstring colourMapPath = (GetExeDir() + wxT("colourMaps")).c_str();
